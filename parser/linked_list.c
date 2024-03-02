c6388373e57ca0d9b9314a5c61e2a7adfbbc9317
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-02 12:40:49 +0100

    update creat node

--- parser/linked_list.c
+++ parser/linked_list.c
@@ -1,13 +1,15 @@
 #include "../included/minishell.h"
 
-t_node	*ft_creat_node_cmd(char *str, t_token tok)
+t_node	*ft_creat_node_cmd(char *str, size_t len, t_token)
 {
 	t_node	*new;
 
 	new = malloc(sizeof(t_node));
 	if (!new)
 		allocate_error(strerror(errno));
-	new->pre_cmd = str;
+	new->pre_cmd = ft_substr(str, 0, len);
+	if (!new)
+		allocate_error(strerror(errno));
 	new->cmd = NULL;
 	new->cmd = tok;
 	new->rchild = NULL;
