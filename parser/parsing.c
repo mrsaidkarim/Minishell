d83952e35a90cff9fd53650132aebb49ca8d6544
SaidKarim-web <sirsaidkarim@gmail.com>
2024-03-10 20:54:39 +0000

    Modify the parssing main file

--- parser/parsing.c
+++ parser/parsing.c
@@ -1,106 +1,140 @@
 #include "../included/minishell.h"
 
-t_token	check_tok(char *str)
+// Return the operand file's name or the command string based on the flag 'red'.
+char	*ft_wordred(char *line, int i, char red)
 {
-	if (str[0] == '|' && str[1] == '|')
-		return (OR);
-	if (str[0] == '&' && str[1] == '&')
-		return (AND);
-	if (str[0] == '>' && str[1] == '>')
-		return (REDIR_APPEND);
-	if (str[0] == '<' && str[1] == '<')
-		return (HEREDOC);
-	if (str[0] == '|')
-		return (PIPE);
-	if (str[0] == ')')
-		return (BRKT_CLOSE);
-	if (str[0] == '(')
-		return (BRKT_OPEN);
-	if (str[0] == '>')
-		return (REDIR_OUT);
-	if (str[0] == '<')
-		return (REDIR_IN);
-	return (EXPR);
-}
-void	print_error_syntax(char *str, t_token tok)
-{
-	if (tok == OR || tok == AND)
-		printf("bash: %s `%c%c'\n", UNEXPECTED_TOK, str[0], str[1]);
-	else
-		printf("bash: %s `%c'\n", UNEXPECTED_TOK, str[0]);
-}
+	int		word_size;
+	int		start;
+	char	*str;
 
-int	check_syntax(char *str)
-{
-	t_token tok;
-
-	tok = check_tok(str);
-	if (tok == PIPE || tok == OR
-		|| tok == AND || tok == BRKT_CLOSE)
+	word_size = 0;
+	start = i;
+	while (line[i] && check_tok(line + i) == EXPR && line[i] != red)
 	{
-		print_error_syntax(str, tok);
-		return (0);
-	}
-	return (1);
-}
-
-int	ft_check_delim(char *str, int i)
-{
-	if (str[i] == '|' && str[i + 1] == '|')
-		return (2);
-	else if (str[i] == '&' && str[i + 1] == '&')
-		return (2);
-	else if (str[i] == '|')
-		return (1);
-	else
-		return (0);
-}
-
-t_node	*build_list(char *line)
-{
-	int		index;
-	t_node	*head;
-	t_node	*tmp;
-	int		len;
-
-	index = 0;
-	head = NULL;
-	while (line[index])
-	{
-		if (ft_check_delim(line, index))
+		if (line[i] == '\'' || line[i] == '\"')
 		{
-			tmp = ft_create_cmd(line + index, ft_check_delim(line, index), check_tok(line + index));
-			ft_add_back(&head, tmp);
-			index += ft_check_delim(line, index);
+			if (ft_protected_word(line, &i, &word_size, line[i]) == -1)
+				return (ft_print_syntax_error("unclosed quotes", NULL, -1), NULL);
+			word_size++;
+			i++;
 		}
 		else
 		{
-			len = index;
-			while (line[len] && ft_check_delim(line, len) == 0)
-				len++;
-			tmp = ft_create_cmd(line + index, len, check_tok(line + index));
-			ft_add_back(&head, tmp);
-			index = len;
+			i++;
+			word_size++;
 		}
 	}
-	return (head);
+	str = ft_substr(line, start, word_size);
+	while (red == ' ' && start < i)
+		line[start++] = 127;
+	return (str);
 }
 
-char *parsing(char *input)
+/* main redirection function: check for errors syntax
+and return redirection node; */
+t_redir	*ft_create_red(char *line, int *i, t_token tok)
 {
-	t_node *head;
-	char *str;
-	t_token tok;
+	char	*file_name;
 
-	str = function_one_space(input);
-	if (!check_syntax(str))
+	line[*i] = 127;
+	(*i)++;
+	if (tok == REDIR_APPEND || tok == HEREDOC)
+		line[(*i)++] = 127;
+	while (ft_isspace(line[*i]))
+		line[(*i)++] = 127;
+	if (!line[*i] || (check_tok(line + (*i)) != EXPR))
+	{
+		if (!line[*i])
+			ft_print_syntax_error("near unexpected token `newline'", NULL, -1);
+		else
+			ft_print_syntax_error("near unexpected token", &line[*i], 2);
 		return (NULL);
-	head = build_list(str);
+	}
+	file_name = ft_wordred(line, *i, ' ');
+	if (!file_name)
+		return (NULL);
+	return (ft_create_rednode(file_name, tok));
+}
+
+// prepare the node command and add it to the linked list
+int	ft_prepare_cmd(char *line, int *i, t_node **head)
+{
+	int		j;
+	char	*cmd;
+	t_redir	*red;
+
+	j = *i;
+	red = NULL;
+	while (line[*i] && !ft_check_delim(line, *i))
+	{
+		if (check_tok(line + (*i)) != EXPR)
+		{
+			if (ft_add_red(&red, ft_create_red(line, i,
+						check_tok(line + (*i)))) == -1)
+				return (-1);
+		}
+		else
+			(*i)++;
+	}
+	cmd = ft_wordred(line, j, '\0');
+	if (!cmd)
+		return (-1);
+	if (ft_add_back(head, ft_create_cmd(cmd, ft_strlen(cmd), EXPR, red)) == -1)
+		return (-1);
+	return (1);
+}
+
+// Tokenizes the input line and builds a linked list of commands with associated tokens and redirections.
+int	ft_build_cmds(char *line, t_node **head)
+{
+	int	i;
+	int	last_brkt_close;
+	int	prev_i;
+
+	i = 0;
+	last_brkt_close = 0;
+	while (line[i])
+	{
+		prev_i = i;
+		if (ft_check_delim(line, i))
+		{
+			if (ft_check_syntax_combination(line, &i, &last_brkt_close,
+					check_tok(line + i)) == -1)
+				return (-1);
+			ft_add_back(head, ft_create_cmd(NULL, prev_i - i,
+					check_tok(line + prev_i), NULL));
+		}
+		else
+		{
+			if (ft_prepare_cmd(line, &i, head) == -1)
+				return (-1);
+		}
+	}
+	return (0);
+}
+
+char	*parsing(char *input)
+{
+	t_node	*head;
+	char	*str;
+	t_token	tok;
+
+	head = NULL;
+	str = ft_clean_input(input);
+	if (!ft_first_check(str))
+		return (NULL);
+	if (ft_build_cmds(str, &head) == -1)
+		return (NULL);
 	while (head)
 	{
-		printf("%s %d\n", head->pre_cmd, head->tok);
+		printf("|%s|, red:", head->pre_cmd);
+		while (head->redirections)
+		{
+			printf("%s|,", head->redirections->file);
+			head->redirections = head->redirections->rchild;
+		}
+		printf("\n");
 		head = head->rchild;
 	}
-	exit(1);
 	return (str);
 }
