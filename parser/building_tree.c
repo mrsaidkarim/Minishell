4e4a7c6e9f8cfd87a61f74a3561dc009e7b3f3d3
SaidKarim-web <sirsaidkarim@gmail.com>
2024-03-19 01:56:21 +0000

    Add implementation of binary tree construction

---
+++ parser/building_tree.c
@@ -0,0 +1,34 @@
+#include "../included/minishell.h"
+
+// Recursively transforms a binary tree according to specified conditions.
+t_node	*ft_tree(t_node *head)
+{
+	if (!head)
+		return (NULL);
+	else if (head->tok == EXPR)
+	{
+		if (head->lchild && head->rchild)
+		{
+			head->rchild->lchild = head->lchild;
+			head->lchild->rchild = head->rchild;
+			if (head->rchild->rchild)
+				head->lchild->rchild = head->rchild->rchild;
+		}
+		head->lchild = NULL;
+		head->rchild = NULL;
+	}
+	else
+	{
+		head->rchild = ft_tree(head->lchild);
+		head->lchild = ft_tree(head->lchild);
+	}
+	return (head);
+}
+
+// Builds a binary tree from a postfix expression represented as a double-linked list.
+void	ft_build_tree(t_node *head)
+{
+	while(head->rchild)
+		head = head->rchild;
+	ft_tree(head);
+}
