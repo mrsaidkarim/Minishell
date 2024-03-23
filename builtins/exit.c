f6bbea701456bf35c66b0339ade0b3f2378d709c
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-23 02:50:32 +0000

    update exit builtin

--- builtins/exit.c
+++ builtins/exit.c
@@ -1,22 +1,41 @@
 #include "../included/minishell.h"
 
+
+void	errors_exit(char *str, int c)
+{
+	ft_putstr_fd("exit\n", 2);
+	ft_putstr_fd("bash: exit: ", 2);
+	if (c == 1)
+		ft_putstr_fd("too many arguments\n", 2);
+	if (c == 2)
+	{
+		ft_putstr_fd(str, 2);
+		ft_putstr_fd(": numeric argument required\n", 2);
+	}
+}
 int	is_digit(char **cmd)
 {
 	int	i;
 	int j;
+	int check;
 
+	check = 0;
 	i = 0;
 	while (cmd[++i])
 	{
 		j = 0;
 		while (cmd[i][j])
 		{
+			if (cmd[i][j] == '-')
+				j++;
 			if (!(cmd[i][j] >= '0' && cmd[i][j] <= '9'))
 				return (0);
 			j++;
 		}
 	}
-	
+	ft_atoll(cmd[1], &check);
+	if (check != 0)
+		return (0);
 	return (1);
 }
 
@@ -45,17 +64,15 @@ void	handle_d_arg(t_var *var,char **cmd)
 	}
 	else if (nbr > 2 && is_digit(cmd))
 	{
-		ft_putstr_fd("exit\n", 2);
-		ft_putstr_fd("bash: exit: too many arguments\n", 2);
+		errors_exit(cmd[1], 1);
 		var->status = 1;
 		return ;
 	}
 	else if (nbr == 2 && is_digit(cmd))
-		exit (ft_atoi(cmd[1]));
+		exit (ft_atoll(cmd[1], &nbr));
 	else
 	{
-		ft_putstr_fd("exit\n", 2);
-		ft_putstr_fd("bash: exit: numeric argument required\n", 2);
+		errors_exit(cmd[1], 2);
 		exit(-1);
 	}
 }
