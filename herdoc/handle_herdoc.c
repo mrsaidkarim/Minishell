989b062b3336f5779c66695c7c8414d3b587aa4b
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-24 22:28:10 +0000

    handle execute herdoc first

---
+++ herdoc/handle_herdoc.c
@@ -0,0 +1,38 @@
+#include "../included/minishell.h"
+
+void	_search_herdoc_(t_node *node, t_var *var)
+{
+	handle_herdoc(node->lchild, var);
+	handle_herdoc(node->rchild, var);
+}
+
+void	_herdoc_(t_node *node, t_var *var)
+{
+	t_redir	*tmp;
+
+	if (node->redirections)
+	{
+		tmp = node->redirections;
+		while (tmp)
+		{
+			if (tmp->tok == HEREDOC)
+			{
+				tmp->fd = ft_heredoc(tmp, var);
+				if (tmp->fd == -1)
+					var->status = 1;
+			}
+			tmp = tmp->rchild;
+		}
+	}
+}
+
+
+void	handle_herdoc(t_node *root, t_var *var)
+{
+	if (!root)
+		return ;
+	if (root->tok != EXPR)
+		return (_search_herdoc_(root, var));
+	else if (root->tok == EXPR)
+		return (_herdoc_(root, var));
+}
