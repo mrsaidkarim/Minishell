46ae55650c8f79761833a8cfd4ebd0125def6c92
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-26 21:47:38 +0000

    update in function fix a bug

--- env/copy_env.c
+++ env/copy_env.c
@@ -12,6 +12,7 @@ void	init_g_var(t_var *var)
 	var->status = 0;
 	var->fd_input = dup(STDIN_FILENO);
 	var->fd_output = dup(STDOUT_FILENO);
+	var->flag = 0;
 }
 
 int	find_char(char *env, char c)
@@ -26,6 +27,29 @@ int	find_char(char *env, char c)
 	}
 	return (-1);
 }
+void	ft_main_env(t_var *var)
+{
+	char	*cwd;
+	char	*tmp;
+	char	*path;
+
+	path = ft_strdup("/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.");
+	cwd = getcwd(NULL, 0);
+	tmp = ft_strjoin("PWD=", cwd);
+	ft_add_env(&var->env, ft_creat_env(tmp, ft_strdup("PWD"), cwd));
+	free(tmp);
+	tmp = ft_strjoin("PATH=", path);
+	ft_add_env(&var->env, ft_creat_env(tmp, ft_strdup("PATH"), path));
+	free(tmp);
+	tmp = ft_strjoin("SHLVL=", "1");
+	ft_add_env(&var->env, ft_creat_env(tmp, ft_strdup("SHLVL"), ft_strdup("1")));
+	free(tmp);
+	tmp = ft_strjoin("_=", "/usr/bin/env");
+	ft_add_env(&var->env, ft_creat_env(tmp, ft_strdup("_"), ft_strdup("/usr/bin/env")));
+	free(tmp);
+	tmp = NULL;
+	var->flag = 1;
+}
 
 void	initialization(t_var *var,char **env)
 {
@@ -35,6 +59,7 @@ void	initialization(t_var *var,char **env)
 	if (!env || !(*env))
 	{
 		ft_putstr_fd("TERM environment variable not set.!\n", 2);
+		ft_main_env(var);
 		return ;
 	}
 	while (*env)
