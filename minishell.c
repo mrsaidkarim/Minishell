21639e0dd7d966c9643876203544a100f0a52713
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-26 02:58:25 +0000

    fix ctr-D

--- minishell.c
+++ minishell.c
@@ -16,21 +16,18 @@ int main(int ac, char **av, char **env)
     {
         ft_signal();
         input = readline("\033[1;32m->Prompt: \033[0m");
-        if (input && input[0])
-        {
-            check_signel = 1;
-            add_history(input);
-            root = parsing(input, &g_var);
-            if (!root)
-                continue;
-            handle_herdoc(root, &g_var);
-            execution(root, &g_var);
-            check_signel = 0;
-            return_in_out_fd(&g_var);
-            freeTree(root);
-        }
-        else
+        if (!input)
             break ;
+        check_signel = 1;
+        add_history(input);
+        root = parsing(input, &g_var);
+        if (!root)
+            continue;
+        handle_herdoc(root, &g_var);
+        execution(root, &g_var);
+        check_signel = 0;
+        return_in_out_fd(&g_var);
+        freeTree(root);
     }
     rl_clear_history();
     return (0);
