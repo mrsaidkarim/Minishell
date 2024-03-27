cda225a83790c43e1d850aa98c17a7b8336b809c
zelabbas <zakariaelabbassi2002@gmail.com>
2024-03-27 23:57:55 +0000

    Update

--- minishell.c
+++ minishell.c
@@ -1,37 +1,54 @@
 #include "included/minishell.h"
 
+int	g_signel = 0;
 
-int check_signel  = 0;
-
-int main(int ac, char **av, char **env)
+void	start_handle(char *input, t_node *root, t_var *g_var)
 {
-    char *input;
-    t_var g_var;
-    t_node *root = NULL;
-    (void)ac;
-    (void)av;
-    // check_signel = 0;
-   initialization(&g_var, env);
-    while (1)
-    {
-        ft_signal();
-        input = readline("\033[1;32m->Prompt: \033[0m");
-        if (!input)
-        {
-            ft_putstr_fd("exit\n", 1);
-            break ;
-        }
-        check_signel = 1;
-        add_history(input);
-        root = parsing(input, &g_var);
-        if (!root)
-            continue;
-        handle_herdoc(root, &g_var);
-        execution(root, &g_var);
-        check_signel = 0;
-        return_in_out_fd(&g_var);
-        free_tree(root);
-    }
-    rl_clear_history();
-    return (0);
+	// t_redir	*tmp;
+	g_signel = 1;
+	add_history(input);
+	root = parsing(input, g_var);
+	if (!root)
+	{
+		// if (g_var->error_herdoc)
+		// {
+		// 	tmp = g_var->error_herdoc;
+		// 	while (tmp)
+		// 	{
+		// 		error_heredoc(tmp);
+		// 		tmp = tmp->rchild;
+		// 	}
+		// }
+		g_signel = 0;
+		return ;
+	}
+	handle_herdoc(root, g_var);
+	execution(root, g_var);
+	g_signel = 0;
+	return_in_out_fd(g_var);
+	free_tree(root);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	char	*input;
+	t_var	g_var;
+	t_node	*root;
+
+	((void)ac, (void)av);
+	initialization(&g_var, env);
+	root = NULL;
+	while (1)
+	{
+		ft_signal();
+		input = readline("\033[1;32m->Prompt: \033[0m");
+		if (!input)
+		{
+			ft_putstr_fd("exit\n", 1);
+			break ;
+		}
+		start_handle(input, root, &g_var);
+	}
+	free_all(&g_var);
+	return (0);
 }
