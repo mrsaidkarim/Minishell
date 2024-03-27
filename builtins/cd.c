855ee70509e59eb3e96ae2e184ebc045344fa93e
SaidKarim-web <sirsaidkarim@gmail.com>
2024-03-27 03:16:24 +0000

    handle some errors in cd builtin

--- builtins/cd.c
+++ builtins/cd.c
@@ -19,8 +19,8 @@ int	ft_oldpwd(t_env *env, char *oldpwd)
 	char	*env_var;
 	char	*tmp;
 
-	if (!ft_env_search(env, "OLDPWD"))
-		return (ft_env_replace(env, "OLDPWD", oldpwd));
+	if (ft_env_search(env, "OLDPWD"))
+		return (ft_env_replace(ft_env_search(env, "OLDPWD"), "OLDPWD", oldpwd));
 	tmp = ft_strjoin("OLDPWD", "=");
 	env_var = ft_strjoin(tmp, oldpwd);
 	ft_add_env(&env, ft_creat_env(env_var, "OLDPWD", oldpwd));
@@ -38,41 +38,45 @@ void	ft_print_cd_error(char *pwd)
 	ft_putstr_fd("\n", 2);
 }
 
-void	ft_update_env(t_var *var, char *pwd, char *oldpwd)
+void	ft_update_env(t_var *var, char *pwd, char *oldpwd, char *tmp_pwd)
 {
 	if (chdir(pwd) != 0)
 	{
 		ft_print_cd_error(pwd);
 		var->status = 1;
+		free(pwd);
 		return ;
 	}
-	pwd = getcwd(NULL, 0);
-	if (!pwd)
-		return (var->status = 1, ft_putstr_fd(strerror(errno), 2));
+	if (!getcwd(tmp_pwd, PATH_MAX))
+	{
+		var->status = 0;
+		ft_print_cd_error("error retrieving current directory: \
+			getcwd: cannot access parent directories");
+	}
+	free(pwd);
+	pwd = strdup(tmp_pwd);
 	if (ft_env_replace(ft_env_search(var->env, "PWD"), "PWD", pwd)
 		|| ft_oldpwd(var->env, oldpwd))
 		ft_putstr_fd("Error updating environment variables\n", 2);
 	free(pwd);
-	free(oldpwd);
 	return ;
 }
 
 void	ft_cd(t_var *var, char **cmd)
 {
 	char	*pwd;
-	char	*oldpwd;
+	char	oldpwd[PATH_MAX];
+	char	tmp_pwd[PATH_MAX];
 
 	if (!cmd[1])
 	{
-		pwd = ft_get_envvar(*var, "HOME");
+		pwd = strdup(ft_get_envvar(*var, "HOME"));
 		if (!pwd)
 			return (var->status = 1,
 				ft_putstr_fd("HOME environment variable is not set", 2));
 	}
 	else
-		pwd = cmd[1];
-	oldpwd = getcwd(NULL, 0);
-	if (!oldpwd)
-		return (var->status = 1, ft_print_cd_error(pwd));
-	ft_update_env(var, pwd, oldpwd);
+		pwd = strdup(cmd[1]);
+	getcwd(oldpwd, PATH_MAX);
+	ft_update_env(var, pwd, oldpwd, tmp_pwd);
 }
