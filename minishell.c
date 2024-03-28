ede0d4a1babc8a9dcf5c94de61cf902c2f1b8f80
SaidKarim-web <sirsaidkarim@gmail.com>
2024-03-28 02:17:57 +0000

    (handle herdoc in case of syntax error): execute the herdoc after diplaying the syntax error message

--- minishell.c
+++ minishell.c
@@ -10,6 +10,7 @@ int main(int ac, char **av, char **env)
     t_node *root = NULL;
     (void)ac;
     (void)av;
+    t_redir *herdoc_error;
     // check_signel = 0;
    initialization(&g_var, env);
     while (1)
@@ -25,7 +26,26 @@ int main(int ac, char **av, char **env)
         add_history(input);
         root = parsing(input, &g_var);
         if (!root)
-            continue;
+        {
+            if (g_var.error_herdoc == NULL)
+                continue;
+            else
+            {
+                printf("\nherdoc ta3 l9lawi\n");
+                herdoc_error = g_var.error_herdoc;
+                while (herdoc_error)
+                {
+                    printf("|%s|\t", herdoc_error->file);
+                    herdoc_error = herdoc_error->rchild;
+                }
+                printf("\n");
+                ft_free_red(g_var.error_herdoc);
+                g_var.error_herdoc = NULL;
+                continue;
+            }
+        }
+        ft_free_red(g_var.error_herdoc);
+        g_var.error_herdoc = NULL;
         handle_herdoc(root, &g_var);
         execution(root, &g_var);
         check_signel = 0;
